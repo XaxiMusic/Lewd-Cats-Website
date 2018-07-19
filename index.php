dc99a21089b13daaefdb93ffb01cbc78e25c5a49
Xaxi <brandon4657@hotmail.com>
2018-07-19 23:31:11 +0100

    Added Model

--- index.php
+++ index.php
@@ -6,6 +6,50 @@
     <link rel="stylesheet" href="https://bootswatch.com/4/slate/bootstrap.css">
     <link href="https://fonts.googleapis.com/css?family=Orbitron:700" rel="stylesheet">
     <link rel="icon" href="/images/logo.png">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+}
+
+/* The Close Button */
+.close {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+}
+</style>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -31,11 +75,56 @@
           <li class="nav-item">
             <a class="nav-link" href="#">Discord</a>
           </li>
-          <li class="nav-item" style="right: 0px;position: absolute;">
+          <li class="nav-item" id="myBtn" style="right: 0px;position: absolute;">
             <a class="nav-link" href="#">PLACE_HOLDER</a>
           </li>
         </ul>
       </div>
     </nav>
+  <div id="myModal" class="modal">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content" style="background-color: #32383e;">
+        <div class="modal-header">
+          <h5 class="modal-title">%PLACE_NAME%'s Profile</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span class="close" aria-hidden="true">&times;
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>All your shit here :D</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script>
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+  </script>
   </body>
 </html>
