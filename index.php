c776d6597fe70a4458c33accf7999f3f44ab0bcf
Xaxi <brandon4657@hotmail.com>
2018-07-20 17:58:56 +0100

    added icon placeholders.

--- index.php
+++ index.php
@@ -107,13 +107,13 @@ body {font-family: Arial, Helvetica, sans-serif;}
     </div>
   </div>
   <div class="mainPage" style="padding-top:5%;margin: 0 auto;width:70%;">
-    <h3 id="progress-animated">Members 7/10</h3>
+    <h3>%icon% Members 7/10</h3>
     <div class="progress">
-      <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 70%"></div>
+      <div class="progress-bar bg-info progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%"></div>
     </div>
   </div>
 
-
+  <script src="inc/xaxi_placeholders.js"></script>
   <script>
   // Get the modal
   var modal = document.getElementById('myModal');
